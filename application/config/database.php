83fc99490ae4ed9cfc78f95a8c7f3be717b80d81
unknown <Blade.Dyer@SL-WL-046.SLO.MBO.AD>
2015-02-14 10:14:25 -0800

    Commting Bootstrap and other thing

--- application/config/database.php
+++ application/config/database.php
@@ -49,9 +49,9 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = '';
-$db['default']['password'] = '';
-$db['default']['database'] = '';
+$db['default']['username'] = 'bladed49_bladed';
+$db['default']['password'] = 'Choco35)';
+$db['default']['database'] = 'bladed49_notesDB';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
