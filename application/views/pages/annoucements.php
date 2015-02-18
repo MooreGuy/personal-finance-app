951933c15c1da1a59cabf99a79d49229bf7a9e39
Blade Dyer <blade.dyer@gmail.com>
2015-02-17 19:26:29 -0800

    uploading pages about_us, admin, annouce, terms, user prof

---
+++ application/views/pages/annoucements.php
@@ -0,0 +1,35 @@
+<?php 
+	$base_url = glob($_SERVER["SERVER_NAME"]);
+?>
+
+<html lang="en"><head>
+		<title>Financial_Sharing</title>
+
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<!-- Bootsrap and our CSS-->
+		<link rel="stylesheet" type="text/css" href="<?php $base_url ?>css/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href="<?php $base_url ?>css/styles.css">
+
+		<!-- JQuery, Bootstrap, CanvasJS, and our JS-->
+		<script type="text/javascript" src="<?php $base_url ?>js/jquery-2.1.1.min.js"></script>
+		<script type="text/javascript" src="<?php $base_url ?>js/bootstrap.min.js"></script>
+		<script type="text/javascript" src="<?php $base_url ?>js/scripts.js"></script>
+		<script type="text/javascript" src="<?php $base_url ?>js/jquery.canvasjs.min.js"></script>
+		
+	</head>
+
+	<body>
+		
+		
+
+
+	      <footer>
+	        <p>© Company 2014</p>
+	      </footer>
+	    </div> <!-- /container -->
+
+	
+	</body>
+
+</html>
