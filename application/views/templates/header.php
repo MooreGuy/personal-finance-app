c222ab25355f50eb1b361732f181a03ec021e91b
Blade Dyer <blade.dyer@gmail.com>
2015-02-17 20:49:20 -0800

    Added comments in the header file

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -22,10 +22,11 @@
 	</head>
 
 	<body>
-		
+		<!-- Top Navbar -->
 		<nav class="navbar navbar-inverse navbar-fixed-top">
 	      <div class="container">
 	        <div class="navbar-header">
+	        <!-- When the view shrinks the navbar will push things into the hamburger menu -->
 	          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
 	            <span class="sr-only">Toggle navigation</span>
 	            <span class="icon-bar"></span>
@@ -34,7 +35,19 @@
 	          </button>
 	          <a class="navbar-brand" href="#">Project name</a>
 	        </div>
+
+	        <!-- Tabs for each section. This will toggle into the hamburger menu -->
 	        <div id="navbar" class="navbar-collapse collapse">
+	        	<ul class="nav navbar-nav">
+	            	<li class="active"><a href="#home">Home</a></li>
+	            	<li><a href="#about">About</a></li>
+	            	<li><a href="#community">Community</a></li>
+
+	            	<!-- When a user sucessfully signs in show this profile tab -->
+	            	<li><a href="#my_profile">My Profile</a></li>
+	          </ul>
+
+	          <!-- Login form. This will toggle into the hamburger menu-->
 	          <form class="navbar-form navbar-right">
 	            <div class="form-group">
 	              <input type="text" placeholder="Email" class="form-control">
@@ -42,7 +55,7 @@
 	            <div class="form-group">
 	              <input type="password" placeholder="Password" class="form-control">
 	            </div>
-	            <button type="submit" class="btn btn-success">Sign in</button>
+	            <button type="submit" class="btn btn-success">Log in</button>
 	          </form>
 	        </div><!--/.navbar-collapse -->
 	      </div>
