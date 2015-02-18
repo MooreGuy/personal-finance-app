2587aa2136ebc7da60f59cd89b647fd188378ada
Blade Dyer <blade.dyer@gmail.com>
2015-02-17 20:33:42 -0800

    Changed the controller to handle header. Removed header from home

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -3,49 +3,7 @@
 	echo $base_url;
 ?>
 
-<html lang="en"><head>
-		<title>Financial_Sharing</title>
 
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-
-		<!-- Bootsrap and our CSS -->
-		<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-		<link rel="stylesheet" type="text/css" href="css/styles.css">
-
-		<!-- JQuery, Bootstrap, CanvasJS, and our JS -->
-		<script type="text/javascript" src="js/jquery-2.1.1.min.js"></script>
-		<script type="text/javascript" src="js/bootstrap.min.js"></script>
-		<script type="text/javascript" src="js/scripts.js"></script>
-		<script type="text/javascript" src="js/jquery.canvasjs.min.js"></script>
-		
-	</head>
-
-	<body>
-		
-		<nav class="navbar navbar-inverse navbar-fixed-top">
-	      <div class="container">
-	        <div class="navbar-header">
-	          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-	            <span class="sr-only">Toggle navigation</span>
-	            <span class="icon-bar"></span>
-	            <span class="icon-bar"></span>
-	            <span class="icon-bar"></span>
-	          </button>
-	          <a class="navbar-brand" href="#">Project name</a>
-	        </div>
-	        <div id="navbar" class="navbar-collapse collapse">
-	          <form class="navbar-form navbar-right">
-	            <div class="form-group">
-	              <input type="text" placeholder="Email" class="form-control">
-	            </div>
-	            <div class="form-group">
-	              <input type="password" placeholder="Password" class="form-control">
-	            </div>
-	            <button type="submit" class="btn btn-success">Sign in</button>
-	          </form>
-	        </div><!--/.navbar-collapse -->
-	      </div>
-	    </nav>
 
 	    <!-- Main jumbotron for a primary marketing message or call to action -->
 	    <div class="jumbotron">
