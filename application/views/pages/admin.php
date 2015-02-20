9ab380fc7c0632c1a90412617d35a5079a365fab
Guy Moore <guy.moore.normal@gmail.com>
2015-02-19 21:16:09 -0800

    Headers for columns laid out.

---
+++ application/views/pages/admin.php
@@ -0,0 +1,26 @@
+<div class="container" >
+	<div class="row" >
+		<div class="col-md-6" >
+			<h1>Announcements</h1>
+			<hr>	
+			<!-- TODO: Add php foreach to pull announcements from database. -->
+			<p>test</p>
+		</div>
+	</div>
+</div>
+
+<div class="container" >
+	<div class="row" >
+		<h1>Members</h1>
+		<hr>	
+	<!-- TODO: Add php foreach to pull members from database. -->
+	</div>
+</div>
+
+<div class="container" >
+	<div class="row" >
+		<h1>Alerts</h1>
+		<hr>	
+	<!-- TODO: Add php foreach to pull alerts from database. -->
+	</div>
+</div>
