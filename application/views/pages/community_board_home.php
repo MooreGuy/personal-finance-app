c225a263a6a0dbc7f80b3fe2a14df7f8bfb794cf
Blade Dyer <blade.dyer@gmail.com>
2015-02-21 08:35:56 -0800

    Added announcement section to community home

---
+++ application/views/pages/community_board_home.php
@@ -0,0 +1,20 @@
+<div class="container">
+
+	<!-- Community Announcements Panel -->
+	<div class="panel panel-info">
+  		<div class="panel-heading">
+
+  			<!-- Making the title a link to the Announcements page -->
+    		<h3 class="panel-title"><a href="#announcements">Community Announcements</a></h3>
+  		</div>
+
+  		<div class="panel-body">
+    		Right now we are working on creating the site. More specifically I am working on the community related pages!
+
+  		</div>
+	</div>
+</div><!-- /.container -->
+
+
+<!-- community_board_home.js -->
+<script type="text/javascript" src="js/community_board_home.js"></script>
