5bce710547386804de4da3b9926b5ca0d03e4917
Blade Dyer <blade.dyer@gmail.com>
2015-02-21 09:44:44 -0800

    Added the different sections in the comm home page.

--- application/views/pages/community_board_home.php
+++ application/views/pages/community_board_home.php
@@ -1,19 +1,77 @@
 <div class="container">
+	<div class="row">
 
-	<!-- Community Announcements Panel -->
-	<div class="panel panel-info">
-  		<div class="panel-heading">
+		<!-- Community Announcements Panel -->
+		<div class="col-md-12">
+			<div class="panel panel-info">
+		  		<div class="panel-heading">
 
-  			<!-- Making the title a link to the Announcements page -->
-    		<h3 class="panel-title"><a href="#announcements">Community Announcements</a></h3>
-  		</div>
+		  			<!-- Title and link to the Announcements page -->
+		    		<h3 class="panel-title"><a href="#announcements">Community Announcements</a></h3>
+		  		</div>
 
-  		<div class="panel-body">
-    		Right now we are working on creating the site. More specifically I am working on the community related pages!
-
-  		</div>
-	</div>
+		  		<div class="panel-body">
+		    		Right now we are working on creating the site. More specifically I am working on the community related pages!
+		  		</div>
+			</div>
+		</div>
+	</div><!-- /.row -->
 </div><!-- /.container -->
+
+<div class="container">
+	<div class="row">
+
+		<!-- User Guides Panel -->
+		<div class="col-md-6">
+			<div class="panel panel-info">
+	  			<div class="panel-heading">
+
+	  				<!-- Title and link to the Announcements page -->
+	    			<h3 class="panel-title"><a href="#community_board_user_guides">Community User Guides</a></h3>
+	  			</div>
+
+	  			<div class="panel-body">
+	    			Our User Guides are full of helpful tips by other members of our community. Come check them out and learn more!
+	  			</div>
+	  		</div>
+	  	</div>
+
+  		<!-- Q & A Panel -->
+  		<div class="col-md-6">
+			<div class="panel panel-info">
+	  			<div class="panel-heading">
+
+	  				<!-- Title and link to the Q & A page -->
+	    			<h3 class="panel-title"><a href="#community_board_forum">Community Q &amp; A Forums</a></h3>
+	  			</div>
+
+		  		<div class="panel-body">
+		    		The Community Q &amp; A Forums are a great place to ask questions and get answers from people in the community.
+		  		</div>
+		  	</div>
+	  	</div>
+	</div><!-- /.row -->
+</div><!-- /.container -->
+
+<div class="container">
+	<div class="row">
+		<div class="col-md-12">
+			<div class="panel panel-warning">
+				<div class="panel-heading">
+					
+					<!-- Title for the Community Rules -->
+					<h3 class="panel-title">Community FAQ's and Rules</h3>
+				</div>
+
+				<div class="panel-body">
+					Do onto others as you would like others to do onto you.
+				</div>
+			</div>
+		</div>
+	</div><!-- /.row -->
+</div><!-- /.container -->
+
+
 
 
 <!-- community_board_home.js -->
