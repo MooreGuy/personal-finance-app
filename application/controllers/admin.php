5a7c12e5259f526dc2e2a768da62dcc63295e068
Guy Moore <guy.moore.normal@gmail.com>
2015-02-21 19:37:57 -0800

    Cleaned up admin, leaving just loading the admin page.

--- application/controllers/admin.php
+++ application/controllers/admin.php
@@ -17,10 +17,9 @@ class Admin extends CI_Controller
 			$data['alerts'] = $this->alertsmodel->getLastTwoAlerts();	
 		*/
 
-		$ipsum = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum';
 
+		//Set the title for the header.
 		$data['title'] = ucfirst($page);	
-		$data['numAlerts'] = '3';
 
 		//load the pages
 		$this->load->view( 'templates/header', $data );
