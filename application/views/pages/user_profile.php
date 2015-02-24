13c40153b18360566580ba9421263209dccb62a4
Blade Dyer <blade.dyer@gmail.com>
2015-02-23 21:10:02 -0800

    Made the user profile and styled it consistant with other pages

--- application/views/pages/user_profile.php
+++ application/views/pages/user_profile.php
@@ -1,17 +1,21 @@
 <div class="container">
-	<div clas="row">
 
-		<!-- User Profile Information Section -->
+
+		
 		<div class="col-md-4">
+			<!-- User Profile Information Section -->
+			<!-- TODO: Affix this panel -->
 			<div class="panel panel-default">
 			  	<div class="panel-body profile-info-section">
 
-			    	<h2>User_Name</h2>
+			    	<h2>User Name</h2>
+			    	<hr>
 
-			    	<h4>Email:</h4>
-			    	<p>User_Email</p>
-			    	<h4></h4>
-			    	<p>User_Email</p>
+			    	<h4 class="user-info">Email:</h4> User Email
+			    	<br>
+			    	<h4 class="user-info">First Name:</h4> First Name
+			    	<br>
+			    	<h4 class="user-info">Last Name:</h4> Last Name
 			  	</div>
 			</div>
 		</div>
@@ -20,10 +24,64 @@
 		<!-- User Profile Data Section -->
 		<div class="col-md-8">
 			<div class="continer">
-				<div 
+
+				<!-- Each row can hold up to 2 categories-->
+				<div class="row">
+					<!-- A single category -->
+					<div class="col-md-6">
+						<div class="panel panel-info">
+							<div class="panel-heading">
+								House
+							</div>
+							<div class="panel-body">
+								<form>
+								  	<div class="form-group">
+								    	<input type="text" class="form-control" placeholder="Enter Expense">
+								  	</div>	
+
+								</form>							
+
+								<div class="button-wrapper">
+									<button class="btn btn-success btn-add-expense" type="submit"><span class="glyphicon glyphicon-plus"></span></button>
+								</div>
+							</div>
+						</div>
+					</div>
+
+					<!-- A single category -->
+					<div class="col-md-6">
+						<div class="panel panel-info">
+							<div class="panel-heading">
+								Car
+							</div>
+							<div class="panel-body">
+								
+
+							</div>
+						</div>
+					</div>
+
+					
+				</div><!-- /.row -->
+
+				<!-- Each row can hold up to 2 categories-->
+				<div class="row">
+					<!-- A single category -->
+					<div class="col-md-6">
+						<div class="panel panel-info">
+							<div class="panel-heading">
+								College
+							</div>
+							<div class="panel-body">
+								
+
+							</div>
+						</div>
+					</div>
+					
+				</div><!-- /.row -->
 			</div><!-- /.container -->
 		</div>
 
-	</div><!-- /.row -->
 </div><!-- /.container -->
 
