861b875e7a1976f8eb6c8d77ab7aa6580bbd3c5e
Roberto Cabrales <guantesvinilio@gmail.com>
2015-02-24 20:47:01 -0800

    Changed heading and added horizontal line

--- application/views/pages/community_board_home.php
+++ application/views/pages/community_board_home.php
@@ -5,7 +5,8 @@
 		<div class="col-md-4">
 			<div class="panel panel-default">
                  <div class="panel-body">
-
+                 		<h2>Community Home</h2>
+                 		<hr>
                              Do onto others as you would like others to do onto you.
                  </div>
              </div>
