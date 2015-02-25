884a23db04c9e6322edabf59b8713ee4625e896b
Roberto Cabrales <guantesvinilio@gmail.com>
2015-02-24 20:29:52 -0800

    Updates to community home

--- application/views/pages/community_board_home.php
+++ application/views/pages/community_board_home.php
@@ -3,7 +3,7 @@
 
 		<!-- Community FAQs and Rules -->
 		<div class="col-md-4">
-			<div class="panel panel-warning">
+			
 				<div class="panel-heading">
 					
 					<!-- Title for the Community Rules -->
@@ -13,7 +13,7 @@
 				<div class="panel-body">
 					Do onto others as you would like others to do onto you.
 				</div>
-			</div>
+			
 		</div>
 
 		<!-- This column is for Announcements, User Guides, and Q&A panels -->
