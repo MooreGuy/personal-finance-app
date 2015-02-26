ab5b4e4d3aef2bd3a04cfac88cdb33df256f4411
Guy Moore <guy.moore.normal@gmail.com>
2015-02-26 15:34:28 -0800

    Added loading of models based on what the user wants to see.

--- application/controllers/admin.php
+++ application/controllers/admin.php
@@ -3,23 +3,41 @@
 class Admin extends CI_Controller
 {
 
-	public function dashboard( $page = 'admin' )
-	{
-	
+	/*
+		Dashboard page to show admin data and options. The display
+		parameter decides how and what gets displayed in the admin
+		panel. Overview will show a small ammount of everything, where
+		selecting a specific data type will show only that type.
+	*/
+	public function dashboard( $display = 'overview' )
+	{	
 		//Load the model.
-		$this->load->model( 'alertsmodel' );
+		switch( $display )
+		{
+			case 'overview':
+				$this->load->model( 'alertsmodel' );
+				$this->load->model( 'notificationsmodel' );
+				$this->load->model( 'forummodel' );
+				break;
+			case 'alerts':	
+				$this->load->model( 'alertsmodel' );
+				break;
+			case 'notifications':
+				$this->load->model( 'notificationsmodel' );
+				break;
+			case 'forum:
+				$this->load->model( 'forummodel' );
+				break;
+			default:
+				show_404();
+				break;
+		}
 
-		/*
-			Keeping database calls out of this until we decide on how
-			we want to store database connection info.
-
-			//call the model's function to get alerts.
-			$data['alerts'] = $this->alertsmodel->getLastTwoAlerts();	
-		*/
-
+		$data['alerts'] = $this->alertsmodel->getLastAlerts( 3 );	
+		
 
 		//Set the title for the header.
-		$data['title'] = ucfirst($page);	
+		$data['title'] = 'Admin Dashboard';	
 
 		//load the pages
 		$this->load->view( 'templates/header', $data );
