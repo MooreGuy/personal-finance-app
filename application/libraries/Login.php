e159cd26a1d6e38c6eb605d67f46b494c1df676d
Guy Moore <guy.moore.normal@gmail.com>
2015-03-04 03:22:37 -0800

    Finished Login library that will handle all user login functions.

--- application/libraries/Login.php
+++ application/libraries/Login.php
@@ -5,8 +5,12 @@ class Login extends CI_Library
 
 	/*
 		Login the user in
+		@param string $email the user's email
+		@param string $password the user's password
+		
+		@return either true if the user successfully authenticated, or false if the authentication failed.
 	*/
-	public function login( $username, $password )
+	public function login( $email, $password )
 	{
 		$CI = & get_instance();		
 
@@ -19,22 +23,37 @@ class Login extends CI_Library
 			return True;
 		}
 
-		//Authenticate the user, and get ID if it succeeds
-		$user_id = $CI->User->authenticate()
+		//Authenticate the user credentials, and get ID if it succeeds
+		$user_id = authenticateUser( $email, $password );
+		if( $user_id != NULL )
+		{
+			$cookieInfo = array
+			(
+					'email' => $email,
+					'password' => $password
+			);
+	
+			$CI->session->userdata($cookieInfo);
 
-		//Then add it to the session cookie
-
-		
-		
+			//All authenticated, return true to show that it successfully completed.
+			return True;
+		}
+		else
+		{
+			return False;
+		}
+	
 	}
 
 	/*
 		Check the cookie for login.
+
+		@return true if the cookie has a login sessiona and is a valid cookie, or otherwise false.
 	*/
 	public function checkCookieLogin()
 	{
-		$CI = & get_instance();
-		
+		//Load the session library from the CI instance.
+		$CI = & get_instance();		
 		$CI->load->library('session');
 		
 		
@@ -47,6 +66,30 @@ class Login extends CI_Library
 		{
 			return False;
 		}		
+	}
+
+	/*
+		@param string $email user email address
+		@param string $password user password
+		
+		@return The user ID if authentication succeeds otherwise NULL
+	*/
+	public function authenticateUser( $email, $password )
+	{
+		$CI = & get_instance();
+		$CI->load->model('User');
+		$CI->load->library('session');
+		
+		//Ask the user model to check if the user and password match an account.
+		if( $user_id = $CI->User->authenticate( $email, $password ) )
+		{
+			return $user_id;
+		}
+		else
+		{
+			//Return null, the user didn't authenticate properly.
+			return;
+		}
 	}
 
 	/*
