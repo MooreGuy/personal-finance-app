0d9a5dc3315fca92b6da06b591b0fb029a1b1afe
Roberto Cabrales <guantesvinilio@gmail.com>
2015-03-05 20:21:36 -0800

    Cleaned up Privacy Policy

--- application/views/pages/privacy_policy.php
+++ application/views/pages/privacy_policy.php
@@ -5,7 +5,7 @@
         <hr>
           
         <div class="span12">
-            <p>This Privacy Policy governs the manner in which Social Pups collects, uses, maintains and discloses information collected from users (each, a "User") of the https://socialpups.com website ("Site"). This privacy policy applies to the Site and all products and services offered by Social Pups.</p>
+            <p>This Privacy Policy governs the manner in which BGR collects, uses, maintains and discloses information collected from users (each, a "User") of the https://socialpups.com website ("Site"). This privacy policy applies to the Site and all products and services offered by BGR.</p>
             <h3>Personal identification information</h3>
             <p>We may collect personal identification information from Users in a variety of ways, including, but not limited to, when Users visit our site, register on the site, fill out a form, and in connection with other activities, services, features or resources we make available on our Site. Users may be asked for, as appropriate, name, email address. We will collect personal identification information from Users only if they voluntarily submit such information to us. Users can always refuse to supply personally identification information, except that it may prevent them from engaging in certain Site related activities.</p>
             <h3>Non-personal identification information</h3>
@@ -13,7 +13,7 @@
             <h3>Web browser cookies</h3>
             <p>Our Site may use "cookies" to enhance User experience. User's web browser places cookies on their hard drive for record-keeping purposes and sometimes to track information about them. User may choose to set their web browser to refuse cookies, or to alert you when cookies are being sent. If they do so, note that some parts of the Site may not function properly.</p>
             <h3>How we use collected information</h3>
-            <p>Social Pups may collect and use Users personal information for the following purposes:</p>
+            <p>BGR may collect and use Users personal information for the following purposes:</p>
             <ul>
             <li>To improve customer service</li>
             <li>Information you provide helps us respond to your customer service requests and support needs more efficiently.</li>
@@ -43,15 +43,15 @@
             <h3>Google Adsense</h3>
             <p>Some of the ads may be served by Google. Google's use of the DART cookie enables it to serve ads to Users based on their visit to our Site and other sites on the Internet. DART uses "non personally identifiable information" and does NOT track personal information about you, such as your name, email address, physical address, etc. You may opt out of the use of the DART cookie by visiting the Google ad and content network privacy policy at <a href="http://www.google.com/privacy_ads.html">http://www.google.com/privacy_ads.html</a></p>
             <h3>Changes to this privacy policy</h3>
-            <p>Social Pups has the discretion to update this privacy policy at any time. When we do, we will revise the updated date at the bottom of this page. We encourage Users to frequently check this page for any changes to stay informed about how we are helping to protect the personal information we collect. You acknowledge and agree that it is your responsibility to review this privacy policy periodically and become aware of modifications.</p>
+            <p>BGR has the discretion to update this privacy policy at any time. When we do, we will revise the updated date at the bottom of this page. We encourage Users to frequently check this page for any changes to stay informed about how we are helping to protect the personal information we collect. You acknowledge and agree that it is your responsibility to review this privacy policy periodically and become aware of modifications.</p>
             <h3>Your acceptance of these terms</h3>
             <p>By using this Site, you signify your acceptance of this policy and terms of service. If you do not agree to this policy, please do not use our Site. Your continued use of the Site following the posting of changes to this policy will be deemed your acceptance of those changes.</p>
             <h3>Contacting us</h3>
             <p>If you have any questions about this Privacy Policy, the practices of this site, or your dealings with this site, please contact us at:</p>
-            <p>Social Pups</p>
-            <p>https://socialpups.com</p>
-            <p>255 W. Fallbrook Ave Ste 103 Fresno, CA 93711</p>
-            <p>559-981-5181</p>
+            <p>BGR</p>
+            <p>https://BGR.blah</p>
+            <p>The Great State of, CA 93401</p>
+            <p>555-555-1234</p>
         </div>
          
     </div><!-- /.row -->
