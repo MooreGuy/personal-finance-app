89898f7c9c4a21ee42409abcefcad23f9437b697
Blade Dyer <blade.dyer@gmail.com>
2015-03-09 20:00:15 -0700

    Added the voting html and CSS to the posts

--- application/views/pages/community_board_forums.php
+++ application/views/pages/community_board_forums.php
@@ -23,71 +23,165 @@
 			<!-- Panel group that holds all of the panels for each post. This group of panels should switch when the active category tag switches-->
 			<div class="panel-group tab-pane active" aria-multiselectable="true" id="transport" role="tabpanel">
 
-			  	<!-- Post Panel -->
-				<div class="panel panel-default forums-panel">
+				<div class="container">
 
-			    	<div class="panel-heading" role="tab">
-			      		<h4 class="panel-title">
-			      			<!-- id of the link should be the id of the post in the DB-->
-			        		<a class="header-link-collapse" id="1" aria-expanded="true">
-			          			Bus tricket prices dropped! <span class="badge pull-right">3</span>
-			        		</a>
-			      		</h4>
-			    	</div>
+					<!-- Each Row contains a post-->
+					<div class="row" data-post='1'>
+						<!-- Wrapper for the up/down vote box-->
+						<div class="col-md-1 post-vote-wrapper">
 
-			    	<!-- Add the id of the post ass a class -->
-			    	<div class="panel-collapse closed-panel 1" role="tabpanel">
-			      		<div class="panel-body">
-			        		 Soon buses will be powered by electricity.
-			      		</div>
-			    	</div>
-			  	</div><!-- END Post Panel-->
+							<div class="row up-vote-row">
+								<div class="col-md-1 up-vote-wrapper">
+									<span class="glyphicon glyphicon-chevron-up"></span>
+								</div>
+							</div>
 
-			  	<!-- Post Panel -->
-				<div class="panel panel-default forums-panel">
+							<div class="row vote-count-row">
+								<div class="col-md-1 positive-count-wrapper">
+									<span class="positive-vote-count">2145</span>
+								</div>
+							</div>
 
-			    	<div class="panel-heading" role="tab">
-			      		<h4 class="panel-title">
-			      			<!-- id of the link should be the id of the post in the DB-->
-			        		<a class="header-link-collapse" id="3" aria-expanded="true">
-			          			How riding my bike to work saved me hundreds on gas last year!<span class="badge pull-right">3</span>
-			        		</a>
-			      		</h4>
-			    	</div>
+							<div class="row down-vote-row">
+								<div class="col-md-1 down-vote-wrapper">
+									<span class="glyphicon glyphicon-chevron-down"></span>
+								</div>
+							</div>
+						</div>
 
-			    	<!-- Add the id of the post ass a class -->
-			    	<div class="panel-collapse closed-panel 3" role="tabpanel">
-			      		<div class="panel-body">
-			        		Gas is expensive. Ride your bike to work instead
-			      		</div>
-			    	</div>
-			  	</div>
-			</div><!-- END Post Panel-->
+						<!-- Post Wrapper-->
+						<div class="col-md-11 post-wrapper">
+						  	<!-- Post Panel -->
+							<div class="panel panel-default forums-panel">
+
+						    	<div class="panel-heading" role="tab">
+						      		<h4 class="panel-title">
+						      			<!-- id of the link should be the id of the post in the DB-->
+						        		<a class="header-link-collapse" id="1" aria-expanded="true">
+						          			Bus tricket prices dropped! <span class="badge pull-right">3</span>
+						        		</a>
+						      		</h4>
+						    	</div>
+
+						    	<!-- Add the id of the post ass a class -->
+						    	<div class="panel-collapse closed-panel 1" role="tabpanel">
+						      		<div class="panel-body">
+						        		 Soon buses will be powered by electricity.
+						      		</div>
+						    	</div>
+						  	</div><!-- END Post Panel-->
+						</div><!-- END Post Wrapper-->
+					</div><!-- END Row-->
+
+
+					<!-- Each Row contains a post-->
+					<div class="row" data-post='1'>
+						<!-- Wrapper for the up/down vote box-->
+						<div class="col-md-1 post-vote-wrapper">
+
+							<div class="row up-vote-row">
+								<div class="col-md-1 up-vote-wrapper">
+									<span class="glyphicon glyphicon-chevron-up"></span>
+								</div>
+							</div>
+
+							<div class="row vote-count-row">
+								<div class="col-md-1 positive-count-wrapper">
+									<span class="positive-vote-count">213</span>
+								</div>
+							</div>
+
+							<div class="row down-vote-row">
+								<div class="col-md-1 down-vote-wrapper">
+									<span class="glyphicon glyphicon-chevron-down"></span>
+								</div>
+							</div>
+						</div>
+
+						<!-- Post Wrapper-->
+						<div class="col-md-11 post-wrapper">
+							<!-- Post Panel -->
+							<div class="panel panel-default forums-panel">
+
+						    	<div class="panel-heading" role="tab">
+						      		<h4 class="panel-title">
+						      			<!-- id of the link should be the id of the post in the DB-->
+						        		<a class="header-link-collapse" id="3" aria-expanded="true">
+						          			How riding my bike to work saved me hundreds on gas last year!<span class="badge pull-right">3</span>
+						        		</a>
+						      		</h4>
+						    	</div>
+
+						    	<!-- Add the id of the post ass a class -->
+						    	<div class="panel-collapse closed-panel 3" role="tabpanel">
+						      		<div class="panel-body">
+						        		Gas is expensive. Ride your bike to work instead
+						      		</div>
+						    	</div>
+						  	</div><!-- END Post Panel-->
+						</div><!-- END Post Wrapper-->
+					</div><!-- END Row-->
+
+				</div><!-- END Container-->
+			</div><!-- END Panel Group-->
 
 			<div class="panel-group tab-pane" aria-multiselectable="true" id="food" role="tabpanel">
 
-			  	<!-- Post Panel -->
-				<div class="panel panel-default forums-panel">
+				<div class="container">
 
-			    	<div class="panel-heading" role="tab">
-			      		<h4 class="panel-title">
-			      			<!-- id of the link should be the id of the post in the DB-->
-			        		<a class="header-link-collapse" id="2" aria-expanded="true">
-			          			There should be an In n Out in SLO! <span class="badge pull-right">3</span>
-			        		</a>
-			      		</h4>
-			    	</div>
+					<!-- Each Row contains a post-->
+					<div class="row" data-post='1'>
+						<!-- Wrapper for the up/down vote box-->
+						<div class="col-md-1 post-vote-wrapper">
 
-			    	<!-- Add the id of the post ass a class -->
-			    	<div class="panel-collapse closed-panel 2" role="tabpanel">
-			      		<div class="panel-body">
-			        		Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-			      			Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-			      			Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-			      			Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-			      		</div>
-			    	</div>
-			  	</div><!-- END Post Panel-->
+							<div class="row up-vote-row">
+								<div class="col-md-1 up-vote-wrapper">
+									<span class="glyphicon glyphicon-chevron-up"></span>
+								</div>
+							</div>
+
+							<div class="row vote-count-row">
+								<div class="col-md-1 positive-count-wrapper">
+									<span class="positive-vote-count">45</span>
+								</div>
+							</div>
+
+							<div class="row down-vote-row">
+								<div class="col-md-1 down-vote-wrapper">
+									<span class="glyphicon glyphicon-chevron-down"></span>
+								</div>
+							</div>
+						</div>
+
+						<!-- Post Group Wrapper-->
+						<div class="col-md-11 post-wrapper">
+						  	<!-- Post Panel -->
+							<div class="panel panel-default forums-panel">
+
+						    	<div class="panel-heading" role="tab">
+						      		<h4 class="panel-title">
+						      			<!-- id of the link should be the id of the post in the DB-->
+						        		<a class="header-link-collapse" id="2" aria-expanded="true">
+						          			There should be an In n Out in SLO! <span class="badge pull-right">3</span>
+						        		</a>
+						      		</h4>
+						    	</div>
+
+						    	<!-- Add the id of the post ass a class -->
+						    	<div class="panel-collapse closed-panel 2" role="tabpanel">
+						      		<div class="panel-body">
+						        		Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+						      			Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+						      			Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+						      			Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+						      		</div>
+						    	</div>
+						  	</div><!-- END Post Panel-->
+						</div><!-- END Post Group Wrapper-->
+					</div><!-- END Row-->
+
+				</div><!-- END Container-->
+
 			</div>
 		</div>
 	</div> <!-- END row -->
