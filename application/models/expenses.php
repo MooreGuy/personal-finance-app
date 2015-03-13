d35f7db032d2d11bf90a219dbe578ef180c01402
Guy Moore <guy.moore.normal@gmail.com>
2015-03-12 21:11:16 -0700

    Expense retrieval with special groupings.

--- application/models/expenses.php
+++ application/models/expenses.php
@@ -19,7 +19,7 @@ class Expenses extends CI_Model
 
 		@param integer $id the particular user that is related to the expenses.
 	
-		@return
+		@return 
 	*/
 	function get_current_by_id( $id )
 	{
@@ -31,6 +31,9 @@ class Expenses extends CI_Model
 		$this->db->query($sql, array($id));
 	}
 	
+	/*
+		Returns all expenses that are current.
+	*/
 	function get_all_current()	
 	{
 		$sql = 'select cost, recurring, type, comment, country, state, city
@@ -57,6 +60,47 @@ class Expenses extends CI_Model
 								$comment, $country, $state, $city ));	
 
 		return $query-result();
+	}
+
+	/*
+		Get all the types and their ids from the expense_types table.
+		
+		@return an array of all the types and their ids
+	*/
+	function get_types()
+	{
+		$sql = 'select id, type from expense_types;';
+		
+		$types = $this->db->query($sql);
+
+		return $types->result_array();
+	}
+
+	/*
+		Return all of the current expenses in an associative array grouped by their type in arrays.
+
+		@return an associative array with the key of the expense type, holding an array of the expenses for that type.
+	*/
+	function get_current_expenses_grouped_by_user( $user_id )
+	{
+		//Query to get the expenses from the database for a specific type.
+		$sql = select cost, interval, comment, location
+					from expenses
+					where user_id = ?
+					and type_id = ?;';
+
+		//Get an array of all the types and their ids.
+		$expense_types = $this->get_types();
+
+		//The array that all the expenses will be thrown into.
+		$grouped_expenses;
+		foreach( $expense_types as $type )
+		{
+			$query = $this->db->query( $sql, array( $user_id, $type->id );
+			$grouped_expenses[$type->id] = $query->result_array();
+		}
+
+		return $expense_types;
 	}
 }
 ?>
