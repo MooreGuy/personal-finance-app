5157ed7f953fc73b6d1b0cb88260f5e0fcad510a
Guy Moore <guy.moore.normal@gmail.com>
2015-04-18 11:12:48 -0700

    Clean up template styling and class names.

--- application/views/pages/admin.php
+++ application/views/pages/admin.php
@@ -1,70 +1,69 @@
 <!-- SIDEBAR -->
 <div class="container">
-  
 
 	<!-- MAIN -->
-    <div class="col-sm-9 col-md-12 main">
-      <h1 class="page-header">Dashboard</h1>
-      <div class="row placeholders">
-		
-      </div>
+	<div class="col-sm-9 col-md-12 main">
+		<h1 class="page-header">Dashboard</h1>
 
-      <h2 class="sub-header">Section title</h2>
-      <div class="table-responsive">
+		<h2 class="sub-header">Section title</h2>
+
+		<section class="table-responsive alerts-table-container">
 
 		<!-- Start of the table. -->
-        <table class="table table-striped">
-			
-				<!-- Table header displaying the column headers. -->
-					<thead>
-						<tr>
-							<?php
-							/*
-								Spit out all the keys from the first element of the
-								alert array into the table header.
-							*/
-								foreach( $alerts[0] as $key => $value )
-								{
-									echo '<th>';
-									echo $key;
-									echo '</th>';
-								}
-							?>
-						</tr>
-					</thead>
+		<table class="table table-striped alerts-table">
+				
+			<!-- Table header displaying the column headers. -->
+			<thead>
+				<tr>
+					<?php
+						/*
+							Spit out all the keys from the first element of the
+							alert array into the table header.
+						*/
+						foreach( $alerts[0] as $key => $value )
+						{ ?>
+							<th>
+								<?php echo $key; ?>
+							</th>
+						<?php
+						}
+					?>
+				</tr>
+			</thead>
 
-					<!-- Start of the main table. -->
-					<tbody>
+			<!-- Start of the main table. -->
+			<tbody>
+
+					<?php
+					/* Fill the main table with alerts. */
+					foreach( $alerts as $alert )
+					{	
+					?>
+						<tr>
 
 						<?php
-							/* Fill the main table with alerts. */
-							foreach( $alerts as $alert )
-							{	
-								//open the table.
-								echo '<tr>';
+						foreach( $alert as $property )
+						{ ?>
+							<td>
+								<?php echo $property;?>
+							</td>
+						<?php } ?>
 
-								foreach( $alert as $property )
-								{	
-									echo '<td>';
-									echo $property;
-									echo '</td>';
-								}
+						</tr>
 
-								//close the table.
-								echo '</tr>';
-							}
-						?>
+					<?php } ?>
 
-					</tbody>
-					<!-- End of the main table. --> 
+			</tbody>
+			<!-- End of the main table. --> 
 
-				</table>
-				<!-- End of the entire table -->
+		</table>
+		<!-- End of the entire table -->
 
-			</div>
-		</div>
-	</div>
+	</section>
+	<!-- End alerts-table-container -->
+
 </div>
+<!-- End of container -->
 
 <!-- admin.js -->
 <script type="text/javascript" src="/js/admin.js"></script>
