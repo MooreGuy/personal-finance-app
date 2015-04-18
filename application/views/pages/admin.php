731d0ca1c93b85faabdc5fd051b9c0368446fa8c
Guy Moore <guy.moore.normal@gmail.com>
2015-04-18 10:24:02 -0700

    Remove old graphs from admin.

--- application/views/pages/admin.php
+++ application/views/pages/admin.php
@@ -6,15 +6,7 @@
     <div class="col-sm-9 col-md-12 main">
       <h1 class="page-header">Dashboard</h1>
       <div class="row placeholders">
-        <div class="col-xs-6 col-sm-4 col-md-4 placeholder">
-          <div id="avg_phone_bill"></div>
-        </div>
-        <div class="col-xs-6 col-sm-4 col-md-4 placeholder">
-          <div id="avg_cost_of_rent"></div>
-        </div>
-        <div class="col-xs-6 col-sm-4 col-md-4 placeholder">
-          <div id="avg_cost_of_dining_out"></div>
-        </div>
+		
       </div>
 
       <h2 class="sub-header">Section title</h2>
