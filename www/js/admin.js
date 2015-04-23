e2af33bec90d91ecbab1c8686547d5abbbb4e740
Guy Moore <guy.moore.normal@gmail.com>
2015-04-23 12:23:18 -0700

    Create factory for creating graphs and their divs.

--- www/js/admin.js
+++ www/js/admin.js
@@ -1,91 +1,84 @@
-$(document).ready(function(){
-    //Add the active class to the navbar on page load
-    $('.navbar-nav > li[name="admin"]').addClass('active');
-
-    var chart = new CanvasJS.Chart("avg_phone_bill", {
-
-    	title:{
-        	text: "Average Cost of Phone Bills per Month"              
-      	},
-
-      	toolTip:{
-        	content: "{label}: ${y}"
-      	},
-      	
-      	data: [//array of dataSeries              
-        { //dataSeries object
-
-        	type: "column",
-        	dataPoints: [
-        		{ label: "Verizon", y: 18 },
-        		{ label: "AT@T", y: 29 },
-        		{ label: "Virgin", y: 40 },                                    
-        		{ label: "Sprint", y: 34 }
-        	]
-       	}],
-
-      	axisY:{
-      		prefix: "$"
-    	}    
-    });
-
-    chart.render();
-
-    var chart = new CanvasJS.Chart("avg_cost_of_rent", {
-
-    	title:{
-        	text: "Average Cost of Phone Bills per Month"              
-      	},
-
-      	toolTip:{
-        	content: "{label}: ${y}"
-      	},
-      	
-      	data: [//array of dataSeries              
-        { //dataSeries object
-
-        	type: "column",
-        	dataPoints: [
-        		{ label: "Verizon", y: 18 },
-        		{ label: "AT@T", y: 29 },
-        		{ label: "Virgin", y: 40 },                                    
-        		{ label: "Sprint", y: 34 }
-        	]
-       	}],
-
-      	axisY:{
-      		prefix: "$"
-    	}    
-    });
-
-    chart.render();
-
-    var chart = new CanvasJS.Chart("avg_cost_of_dining_out", {
-
-    	title:{
-        	text: "Average Cost of Phone Bills per Month"              
-      	},
-      	
-		toolTip:{
-        	content: "{label}: ${y}"
-      	},
-
-      	data: [//array of dataSeries              
-        { //dataSeries object
-
-        	type: "column",
-        	dataPoints: [
-        		{ label: "Verizon", y: 18 },
-        		{ label: "AT@T", y: 29 },
-        		{ label: "Virgin", y: 40 },                                    
-        		{ label: "Sprint", y: 34 }
-        	]
-       	}],
-
-      	axisY:{
-      		prefix: "$"
-    	}    
-    });
-
-    chart.render();
+$(document).ready(function() {
 });
+
+var GraphFactory = {
+
+	rowNumber: 0,
+	graphColumnNumber: 0,
+
+	/**
+	 * 	Create a graph div or row every three columns, then return the created div.
+	 */
+	createGraphDiv: function() {
+
+		this.graphColumnNumber++;
+		console.log(this.graphColumnNumber);
+
+		if( this.graphColumnNumber-1 % 3 === 0 ) {
+			this.rowNumber++;
+			var graphRow = $('<div class="row graph-row' + this.rowNumber + '"></div>');
+			$(graphRow).insertAfter('.page-header');
+		}
+		console.log(this.rowNumber);
+
+		var graphColumn = $('<div class="col-xs-6 col-sm-4 col-md-4 " id="avg_phone_bill' + this.graphColumnNumber + '"></div>');
+		$('.graph-row' + this.rowNumber).append(graphColumn);
+
+		$(graphColumn).append('<div class="graph" id="graph' + this.graphColumnNumber + '"></div>');
+
+		return 'graph' + this.graphColumnNumber;
+	},
+
+	/**
+	 *	Get all of the graph data then start creating the graphs.
+	 */
+	getGraphData: function( ) {
+
+		$.ajax({
+			url: window.location.origin + "/graph/get_graph",
+			dataType: "json",
+		}).done(function( data ) {	
+		});
+	},
+
+	insertGraph: function( graphData, graphID ) {
+
+			var chart = new CanvasJS.Chart( graphID, {
+
+			title: {
+				text: graphData['title'] 
+			},
+
+			toolTip:{
+				content: "{label}: ${y}"
+			},
+			
+			data: [//array of dataSeries              
+			{ //dataSeries object
+
+				type: "column",
+				dataPoints: [
+					{ label: "Verizon", y: 10 },
+					{ label: "AT@T", y: 29 },
+					{ label: "Virgin", y: 40 }, 
+					{ label: "Sprint", y: 34 }
+				]
+			}],
+
+			axisY:{
+				prefix: "$"
+			}    
+		});
+
+		chart.render();
+	}
+		
+	
+}
+
+var graphDiv = GraphFactory.createGraphDiv();
+console.log(graphDiv);
+GraphFactory.insertGraph( 'type', graphDiv  );
+
+//Add the active class to the navbar on page load
+$('.navbar-nav > li[name="admin"]').addClass('active');
