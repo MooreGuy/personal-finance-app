9dd982fa2110d39884a8d5cf3014574219115ccf
= <blade.dyer@gmail.com>
2015-05-17 00:00:59 -0700

    Fixed finale merge conflicts with the css.

--- application/views/pages/user_profile.php
+++ application/views/pages/user_profile.php
@@ -128,7 +128,10 @@
 		      			</div>	
 		      		</div>
 	      		</div>
-      		</div>	
+      		</div>
+      		
+      		
+        		
       		
       		<div class="modal-footer">
     			<button type="button" class="btn btn-default pull-left" data-dismiss="modal">Close</button>
