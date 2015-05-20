a10ddbd462c6c0c5fe3f47a9f41431a3b11f4275
Guy Moore <guy.moore.normal@gmail.com>
2015-05-19 21:29:07 -0700

    Wait for DOM to be ready.

--- www/js/graphBanner.js
+++ www/js/graphBanner.js
@@ -1,15 +1,17 @@
-//Initalize and call the owl-carousel to show the graphs
-$(".owl-carousel").owlCarousel({
-	items:3,
-	loop:false,
+$("document").ready(function() {
+	//Initalize and call the owl-carousel to show the graphs
+	$(".owl-carousel").owlCarousel({
+		items:3,
+		loop:false,
+	});
+
+	// Go to the next item
+	$('.graphRightBtn').on("click", function() {
+		$(".owl-carousel").trigger('next.owl.carousel');
+	})
+
+	// Go to the previous item
+	$('.graphLeftBtn').on("click", function() {
+		$(".owl-carousel").trigger('prev.owl.carousel');
+	})
 });
-
-// Go to the next item
-$('.graphRightBtn').on("click", function() {
-	$(".owl-carousel").trigger('next.owl.carousel');
-})
-
-// Go to the previous item
-$('.graphLeftBtn').on("click", function() {
-	$(".owl-carousel").trigger('prev.owl.carousel');
-})
