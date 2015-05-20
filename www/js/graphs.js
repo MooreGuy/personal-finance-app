a4fe59c69868c48c9f470d227012a64123c0ac61
Guy Moore <guy.moore.normal@gmail.com>
2015-05-19 21:06:26 -0700

    Move graph javascript to its own file.

---
+++ www/js/graphs.js
@@ -0,0 +1,76 @@
+var GraphFactory = {
+
+	rowNumber: 0,
+	graphColumnNumber: 0,
+
+	/**
+	 * 	Create a graph div or row every three columns, then return the created div.
+	 */
+	createGraphDiv: function() {
+
+		this.graphColumnNumber++;
+		console.log(this.graphColumnNumber);
+
+		if( this.graphColumnNumber-1 % 3 === 0 ) {
+			this.rowNumber++;
+			var graphRow = $('<div class="row graph-row' + this.rowNumber + '"></div>');
+			$(graphRow).insertAfter('.page-header');
+		}
+		console.log(this.rowNumber);
+
+		var graphColumn = $('<div class="col-xs-6 col-sm-4 col-md-4 " id="avg_phone_bill' + this.graphColumnNumber + '"></div>');
+		$('.graph-row' + this.rowNumber).append(graphColumn);
+
+		$(graphColumn).append('<div class="graph" id="graph' + this.graphColumnNumber + '"></div>');
+
+		return 'graph' + this.graphColumnNumber;
+	},
+
+	/**
+	 *	Get all of the graph data then start creating the graphs.
+	 */
+	getGraphData: function( ) {
+
+		$.ajax({
+			url: window.location.origin + "/graph/",
+			dataType: "json",
+		}).done(function( data ) {	
+		});
+	},
+
+	insertGraph: function( graphData, graphID ) {
+
+			var chart = new CanvasJS.Chart( graphID, {
+
+			title: {
+				text: graphData['title'] 
+			},
+
+			toolTip:{
+				content: "{label}: ${y}"
+			},
+			
+			data: [//array of dataSeries              
+			{ //dataSeries object
+
+				type: "column",
+				dataPoints: [
+					{ label: "Verizon", y: 10 },
+					{ label: "AT@T", y: 29 },
+					{ label: "Virgin", y: 40 }, 
+					{ label: "Sprint", y: 34 }
+				]
+			}],
+
+			axisY:{
+				prefix: "$"
+			}    
+		});
+
+		chart.render();
+	}	
+}
+
+var graphDiv = GraphFactory.createGraphDiv();
+console.log(graphDiv);
+GraphFactory.insertGraph( 'type', graphDiv  );
